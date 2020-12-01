aef824c3a26f5c3e281617c736b4e2c9a01ee765
KariukiAbel <abelmuriithi@gmail.com>
2020-12-01 16:16:18 +0300

    Buttons added successfully

--- calprogram.py
+++ calprogram.py
@@ -4,12 +4,13 @@ import parser
 root = Tk()
 root.title('Calculator')
 
-#adding the input field
+# adding the input field
 display = Entry(root)
-display.grid(row=1, columnspan=6, sticky=W+E)
+display.grid(row=1, columnspan=6, sticky=W + E)
 
-#adding buttons to the calculator
+# adding buttons to the calculator
 Button(root, text="1").grid(row=2, column=0)
-
+Button(root, text="2").grid(row=2, column=1)
+Button(root, text="3").grid(row=2, column=2)
 
 root.mainloop()
